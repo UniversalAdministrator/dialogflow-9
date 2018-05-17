1ad156a7a6dcb597e1a4a036cdeb892bf180d4a5
khanzafir <35225674+khanzafir@users.noreply.github.com>
2018-05-17 10:29:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 var routes = require('./API/Routes/Routes'); //importing route
 routes(server); //register the route
-server.listen((process.env.PORT || 9002), function () {
+server.listen((process.env.PORT || 8000), function () {
     console.log("Server is up and listening on port" + process.env.PORT);
 });
